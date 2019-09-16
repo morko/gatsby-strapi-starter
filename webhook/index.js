ced3a30653a3f0ab46b831461e8c1e52c17d0426
Oskari Pöntinen <mail.morko@gmail.com>
2019-09-16 16:49:01 +0300

    remove downtime of the website when building
    - fixes the downtime when gatsby is being rebuilt by building the
      project and the using `rsync` to synchronize   the built directory
      into the public directory

--- webhook/index.js
+++ webhook/index.js
@@ -1,32 +1,26 @@
-const path = require("path");
-const exec = require("child_process").exec;
 const { WebhookServer } = require("simple-webhooks");
+const dependencies = require("./dependencies");
+const build = require("./build");
 
 const SECRET = process.env.WEBHOOK_SECRET || "bad-secret-remove-immediately!";
-const REPO_PATH = path.join(__dirname, "..");
-const BUILD_CMD = "npm run build";
 
 const webhookServer = new WebhookServer({
   secret: SECRET,
   job: data => buildGatsby(data),
 });
 
-function buildGatsby() {
-  return new Promise((resolve, reject) => {
-    console.log("STARTING BUILD");
-    exec(
-      BUILD_CMD,
-      {
-        cwd: REPO_PATH,
-      },
-      (error, stdout, stderr) => {
-        console.log(stdout);
-        console.error(stderr);
-        console.log("FINISHED BUILD");
-        resolve();
-      }
-    );
-  });
+webhookServer.listen();
+
+async function bootstrap() {
+  await dependencies();
+  await build();
 }
 
-webhookServer.listen();
+bootstrap()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch(err => {
+    console.log(err);
+    process.exit(1);
+  });
